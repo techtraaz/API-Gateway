a078412e09fe2d0e9f6007161bebacc63aa48ea8
techtraaz <techtraazsl@gmail.com>
2026-01-16 15:07:06 +0530

    added user service interface methods

--- src/main/java/com/apigateway/service/UserService.java
+++ src/main/java/com/apigateway/service/UserService.java
@@ -1,4 +1,15 @@
 package com.apigateway.service;
 
+import com.apigateway.dto.UserDto;
+import com.apigateway.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    User createUser(UserDto user);
+    User getUserById(String id);
+    List<User> getAllUsers();
+    void deleteUser(String id);
+
 }
