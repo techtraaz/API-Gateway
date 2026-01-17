0391f300751f7e878eafa3427c329ac446321952
techtraaz <techtraazsl@gmail.com>
2026-01-17 23:05:14 +0530

    added api key service interface

---
+++ src/main/java/com/apigateway/service/ApiKeyService.java
@@ -0,0 +1,14 @@
+package com.apigateway.service;
+
+import com.apigateway.dto.ApiKeyDto;
+import com.apigateway.entity.ApiKey;
+
+import java.util.List;
+
+public interface ApiKeyService {
+
+    ApiKey createApiKey(ApiKeyDto apiKeyDto);
+    ApiKey getApiKeyByValue(String apikey);
+    List<ApiKey> getAllApiKeys();
+
+}
