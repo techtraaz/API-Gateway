faef87efba7201fcf83bf9c4cb5da360bf07af76
techtraaz <techtraazsl@gmail.com>
2026-01-21 14:31:07 +0530

    added api def controller endpoints

--- src/main/java/com/apigateway/controller/ApiDefController.java
+++ src/main/java/com/apigateway/controller/ApiDefController.java
@@ -1,13 +1,12 @@
 package com.apigateway.controller;
 
 import com.apigateway.dto.ApiDefDto;
+import com.apigateway.dto.ResponseBean;
 import com.apigateway.entity.ApiDefinition;
 import com.apigateway.service.ApiDefService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/apidef")
@@ -17,8 +16,28 @@ public class ApiDefController {
     private final ApiDefService apiDefService;
 
     @PostMapping("/define")
-    public ApiDefinition defineApi(@RequestBody ApiDefDto apiDef) {
+    public ResponseEntity<ResponseBean> defineApi(@RequestBody ApiDefDto apiDef) {
         return apiDefService.addNewApi(apiDef);
+    }
+
+    @GetMapping("/get/{apiKey}")
+    public ResponseEntity<ResponseBean> getApiById(@PathVariable String apiKey) {
+        return apiDefService.getApiById(apiKey);
+    }
+
+    @GetMapping("/get/{apiName}")
+    public ResponseEntity<ResponseBean> getApiByName(@PathVariable String apiName) {
+        return apiDefService.getApiByName(apiName);
+    }
+
+    @GetMapping("/get/all")
+    public ResponseEntity<ResponseBean> getAllApi() {
+        return apiDefService.getAllApi();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ResponseBean> deleteApi(@PathVariable String id) {
+        return apiDefService.deleteApiById(id);
     }
 
 
