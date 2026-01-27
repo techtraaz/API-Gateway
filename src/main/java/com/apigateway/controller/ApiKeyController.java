6d7d65a20b83fd0c9c8d0059775598a7ee1f7576
techtraaz <techtraazsl@gmail.com>
2026-01-27 08:49:55 +0530

    added api key controller endpoints

--- src/main/java/com/apigateway/controller/ApiKeyController.java
+++ src/main/java/com/apigateway/controller/ApiKeyController.java
@@ -7,10 +7,7 @@ import com.apigateway.entity.ApiKey;
 import com.apigateway.service.ApiKeyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/apikey")
@@ -28,6 +25,48 @@ public class ApiKeyController {
         responseBean.setData(apikey);
         return ResponseEntity.ok(responseBean);
     }
+
+
+
+    @GetMapping("/get/{apiKey}")
+    public ResponseEntity<ResponseBean> getApiKey(@PathVariable("apiKey") String apiKey){
+        ResponseBean responseBean = new ResponseBean();
+        responseBean.setStatus("success");
+        responseBean.setMessage("Retrieved API key");
+        responseBean.setData(apiKeyService.getApiKeyByValue(apiKey));
+        return ResponseEntity.ok(responseBean);
+    }
+
+
+    @GetMapping("/get/all")
+    public ResponseEntity<ResponseBean> getAllApiKey(){
+        ResponseBean responseBean = new ResponseBean();
+        responseBean.setStatus("success");
+        responseBean.setMessage("Retrieved all API keys");
+        responseBean.setData(apiKeyService.getAllApiKeys());
+        return ResponseEntity.ok(responseBean);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ResponseBean> deleteApiKey(@PathVariable("id") String id){
+        ResponseBean responseBean = new ResponseBean();
+        responseBean.setStatus("success");
+        responseBean.setMessage("Deleted API key");
+        responseBean.setData(apiKeyService.deleteApiKey(id));
+        return ResponseEntity.ok(responseBean);
+    }
+
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseBean> updateApiKey(@RequestBody ApiKeyDto apiKeyDto){
+        ResponseBean responseBean = new ResponseBean();
+        responseBean.setStatus("success");
+        responseBean.setMessage("Updated API key");
+        responseBean.setData(apiKeyService.updateApiKey(apiKeyDto));
+        return ResponseEntity.ok(responseBean);
+    }
+
+
 
 
 }
